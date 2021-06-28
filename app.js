62dad2026b733d305fb5f7b14d80247f4a4f4eaa
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2021-06-29 00:06:38 +0500

    JS file

--- app.js
+++ app.js
@@ -1,32 +1,49 @@
 function add() {
     var data = document.getElementById('text').value;
-if (data === "") {
+
+    if (data === "") {
         swal("Nothing To Add!", "", "warning");
 
     }
- else {
-var div = document.createElement("div")
- div.setAttribute('class', 'input-group p-3')
-  var inp = document.createElement("input");
+    else {
+
+        var div = document.createElement("div")
+        div.setAttribute('class', 'input-group p-3')
+
+        var inp = document.createElement("input");
         inp.value = data
- inp.setAttribute('class', 'form-control bg-darl')
+        inp.setAttribute('class', 'form-control bg-darl')
         inp.setAttribute('disabled', 'disabled')
         // inp.setAttribute('autofocus', 'autofocus')
-  var editBtn = document.createElement('button')
-             var edittext = document.createTextNode('Edit')
+
+
+        var editBtn = document.createElement('button')
+        var edittext = document.createTextNode('Edit')
+        editBtn.appendChild(edittext)
+        editBtn.setAttribute('class', 'btn btn-success')
+        editBtn.setAttribute('onclick', 'edit(this.previousSibling)')
+
+
         var delBtn = document.createElement('button')
+        var deltext = document.createTextNode('Delete')
         delBtn.appendChild(deltext)
-  delBtn.setAttribute('class', 'btn btn-outline-dark')
+        delBtn.setAttribute('class', 'btn btn-outline-dark')
         delBtn.setAttribute('onclick', 'del(this)')
-  div.appendChild(delBtn)
+
+
+        div.appendChild(inp)
+        div.appendChild(editBtn)
+        div.appendChild(delBtn)
         main.appendChild(div)
-       document.getElementById('text').value = "";
+
+        document.getElementById('text').value = "";
         swal("Successfully Added!", "", "success");
 
     }
 }
 
 function del(a) {
+
 swal({
   title: "Are you sure?",
   text: "",
@@ -40,30 +57,33 @@ swal({
     a.parentNode.remove();
   } 
 });
- }
+
+}
 
 
 function edit(inputdata) {
-     var check = inputdata.nextSibling.innerText
+    var check = inputdata.nextSibling.innerText
     if (check === "Edit") {
         console.log(inputdata)
 
         inputdata.disabled = false;
-    
-            inputdata.focus();
-      inputdata.nextSibling.innerText = "Update"
+        inputdata.focus();
+
+        inputdata.nextSibling.innerText = "Update"
         console.log(inputdata.nextSibling.innerText = "Update")
-       }
+    }
     else if (check === "Update") {
         inputdata.disabled = true;
-    
-       inputdata.nextSibling.innerText = "Edit"
+        inputdata.nextSibling.innerText = "Edit"
         swal("Successfully Updated!", "", "success");
-      }
+
+    }
     else {
         alert("Some Error Occur")
     }
-    
+
+}
+
 // alertify.confirm("This is a confirm dialog.",
 //     function () {
 //         alertify.success('Ok');
@@ -72,7 +92,11 @@ function edit(inputdata) {
 //         alertify.error('Cancel');
 //     });
 
-    
+function enter(code) {
+    if (code.keyCode === 13) {
+        add()
+    }
+}
     
     
     
