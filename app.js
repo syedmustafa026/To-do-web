a26e339b4fb9d543b362a2a5d1bb865996bed9a5
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2021-06-29 00:06:05 +0500

    Update app.js

--- app.js
+++ app.js
@@ -64,6 +64,14 @@ function edit(inputdata) {
         alert("Some Error Occur")
     }
     
+// alertify.confirm("This is a confirm dialog.",
+//     function () {
+//         alertify.success('Ok');
+//     },
+//     function () {
+//         alertify.error('Cancel');
+//     });
+
     
     
     
